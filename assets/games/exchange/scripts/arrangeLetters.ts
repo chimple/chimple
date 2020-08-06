7a14799b75a598d60797e77299b4741531bba36e
Srikrishna G <srikrishna@sutara.org>
2020-08-06 10:18:00 +0530

    rearrange letters game
    dynamic loading,drag

---
+++ assets/games/exchange/scripts/arrangeLetters.ts
@@ -0,0 +1,68 @@
+import Drag from "../../../common/scripts/drag";
+import DropCow from "./dropObj";
+import DragHay from "./dragObj";
+import { Util } from "../../../common/scripts/util";
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class ArrangeLetters extends cc.Component {
+  
+  @property(cc.Prefab)
+  playground: cc.Prefab = null;
+  
+  @property(cc.Prefab)
+  playground2: cc.Prefab = null;
+  
+  @property(cc.Prefab)
+  ball2: cc.Prefab = null;
+
+  @property(cc.Prefab)
+  ball: cc.Prefab = null;
+
+  data: Array<Array<string>> = [
+    ["1", "1", "1", "playground", "ball", "cat"],
+    ["1", "1", "1", "playground2", "ball2", "cat"],
+  ];
+  level: string;
+  worksheet: string;
+  problem: string;
+  backgroundName: string;
+  objectName: string;
+  word: string;
+  correctLetterArray:Array<string>
+  
+  onLoad() {
+    [
+      this.level,
+      this.worksheet,
+      this.problem,
+      this.backgroundName,
+      this.objectName,
+      this.word,
+    ] = this.data[1];
+   
+    this.correctLetterArray = this.word.split('')
+    this.loadBackground();
+    this.makeDragObjects();
+
+  }
+
+  makeDragObjects() {
+    var shuffledArray = Util.shuffle(this.correctLetterArray)
+    for (let i = 0; i < this.correctLetterArray.length; i++) {
+      let dragObj = cc.instantiate(this[this.objectName]);
+      dragObj.parent = this.node;
+      dragObj.position =cc.v3(-cc.winSize.width/3 + i*250 ,-cc.winSize.height/4 );
+      dragObj.getChildByName('objLabel').getComponent(cc.RichText).string = shuffledArray[i];
+      cc.log(dragObj.children);
+    }
+  }
+   
+  loadBackground(){
+    let loadbg = cc.instantiate(this[this.backgroundName]);
+    this.node.addChild(loadbg)
+   }
+
+
+}
---
+++ assets/games/exchange/scripts/dragObj.ts
@@ -0,0 +1,59 @@
+import Drag from "../../../common/scripts/drag";
+import DropObj from "./dropObj";
+import { Util } from "../../../common/scripts/util";
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class DragObj extends Drag {
+    @property(cc.AudioClip)
+    correctClip: cc.AudioClip = null;
+
+    @property(cc.RichText)
+    richTextNode: cc.RichText = null;
+
+    private _text: string = null;
+    private _soundName: string = null;
+    private _soundClip: cc.AudioClip = null;
+
+    get text(): string {
+        return this._text;
+    }
+
+    set text(newVal) {
+        this._text = newVal;
+        this.richTextNode.string = newVal;
+    }
+
+    get soundName(): string {
+        return this._soundName;
+    }
+
+    set soundName(newVal) {
+        this._soundName = newVal;
+        Util.loadGameSound(this._soundName, (clip) => {
+            this._soundClip = clip;
+        })
+    }
+
+
+    onTouchStart(touch: cc.Touch) {
+        super.onTouchStart(touch);
+        if(this._soundClip != null) {
+            Util.play(this._soundClip, false);
+        }
+    }
+
+    onTouchEnd(touch: cc.Touch) {
+        super.onTouchEnd(touch);
+        if(this.match) {
+            if(this._soundClip != null) {
+                Util.play(this._soundClip, false);
+            }
+        } else {
+            this.node.emit('dragHayWrong')
+        }
+    }
+
+
+}
