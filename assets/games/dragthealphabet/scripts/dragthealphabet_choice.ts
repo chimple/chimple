21a3aa60ff754ff8afcce45ef3dc5b5bb259d64c
nikhil <35517618+nikhil-chimple@users.noreply.github.com>
2020-08-10 22:03:43 +0530

    added animations

--- assets/games/dragthealphabet/scripts/dragthealphabet_choice.ts
+++ assets/games/dragthealphabet/scripts/dragthealphabet_choice.ts
@@ -6,13 +6,20 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class DragTheAlphabetChoice extends Drag {
+
+    @catchError()
+    onTouchStart(touch: cc.Touch) {
+        super.onTouchStart(touch);
+        this.node.emit('DragTheAlphabetOnTouch');
+    }
+
     @catchError()
     onTouchEnd(touch: cc.Touch) {
-        super.onTouchEnd(touch)
+        super.onTouchEnd(touch);
         if (this.match) {
-            this.node.emit('DragTheAlphabetMatch', this);
+            this.node.emit('DragTheAlphabetChoiceMatch', this);
         } else {
-            this.node.emit('DragTheAlphabetNoMatch');
+            this.node.emit('DragTheAlphabetChoiceNoMatch');
         }
     }
 }
