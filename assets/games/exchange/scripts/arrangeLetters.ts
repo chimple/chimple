ba172a3ae7e1b4e33d703d793325f17b9dc6bb67
Srikrishna G <srikrishna@sutara.org>
2020-08-10 10:10:41 +0530

    Collider Implementation & data change.

--- assets/games/exchange/scripts/arrangeLetters.ts
+++ assets/games/exchange/scripts/arrangeLetters.ts
@@ -1,22 +1,18 @@
-import Drag from "../../../common/scripts/drag";
-import DropCow from "./dropObj";
-import DragHay from "./dragObj";
 import { Util } from "../../../common/scripts/util";
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class ArrangeLetters extends cc.Component {
-  
   @property(cc.Prefab)
-  dropObj: cc.Prefab = null
- 
+  dropObj: cc.Prefab = null;
+
   @property(cc.Prefab)
   playground: cc.Prefab = null;
-  
+
   @property(cc.Prefab)
   playground2: cc.Prefab = null;
-  
+
   @property(cc.Prefab)
   ball2: cc.Prefab = null;
 
@@ -24,8 +20,8 @@ export default class ArrangeLetters extends cc.Component {
   ball: cc.Prefab = null;
 
   data: Array<Array<string>> = [
-    ["1", "1", "1", "playground", "ball", "cat"],
-    ["1", "1", "1", "playground2", "ball2", "cat"],
+    ["1", "1", "1", "playground", "ball", "c*a*t*t*l*e"],
+    ["1", "1", "1", "playground2", "ball2", "c*a*t"],
   ];
   level: string;
   worksheet: string;
@@ -33,8 +29,8 @@ export default class ArrangeLetters extends cc.Component {
   backgroundName: string;
   objectName: string;
   word: string;
-  correctLetterArray:Array<string>;
-  
+  correctLetterArray: Array<string>;
+
   onLoad() {
     [
       this.level,
@@ -44,28 +40,31 @@ export default class ArrangeLetters extends cc.Component {
       this.objectName,
       this.word,
     ] = this.data[1];
-   
-    this.correctLetterArray = this.word.split('')
+
+    this.correctLetterArray = this.word.split("*");
+    cc.log("<>" + this.correctLetterArray);
     this.loadBackground();
     this.makeDragObjects();
-
   }
 
   makeDragObjects() {
-    var shuffledArray = Util.shuffle(this.correctLetterArray)
+    var shuffledArray = Util.shuffle(this.correctLetterArray);
     for (let i = 0; i < this.correctLetterArray.length; i++) {
       let dragObj = cc.instantiate(this[this.objectName]);
       dragObj.parent = this.node;
-      dragObj.position =cc.v3(-cc.winSize.width/3 + i*250 ,-cc.winSize.height/4 );
-      dragObj.getChildByName('objLabel').getComponent(cc.RichText).string = shuffledArray[i];
+      dragObj.name = shuffledArray[i];
+      dragObj.position = cc.v3(
+        -cc.winSize.width / 3 + i * 250,
+        -cc.winSize.height / 4
+      );
+      dragObj.getChildByName("objLabel").getComponent(cc.Label).string =
+        shuffledArray[i];
       cc.log(dragObj.children);
     }
   }
-   
-  loadBackground(){
+
+  loadBackground() {
     let loadbg = cc.instantiate(this[this.backgroundName]);
-    this.node.addChild(loadbg)
-   }
-
-
+    this.node.addChild(loadbg);
+  }
 }
--- assets/games/exchange/scripts/dragObj.ts
+++ assets/games/exchange/scripts/dragObj.ts
@@ -1,68 +1,76 @@
 import Drag from "../../../common/scripts/drag";
-import DropObj from "./dropObj";
+import ArrangeLetters from "./arrangeLetters";
 import { Util } from "../../../common/scripts/util";
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class DragObj extends Drag {
-    @property(cc.AudioClip)
-    correctClip: cc.AudioClip = null;
+  @property(cc.AudioClip)
+  correctClip: cc.AudioClip = null;
 
-    @property(cc.RichText)
-    richTextNode: cc.RichText = null;
+  @property(cc.RichText)
+  richTextNode: cc.RichText = null;
 
-    private _text: string = null;
-    private _soundName: string = null;
-    private _soundClip: cc.AudioClip = null;
+  private _text: string = null;
+  private _soundName: string = null;
+  private _soundClip: cc.AudioClip = null;
 
-    get text(): string {
-        return this._text;
+  isCollisionEnable: boolean = false;
+  originalLocation: cc.Vec2;
+  onLoad() {
+    cc.director.getCollisionManager().enabled = true;
+    cc.director.getCollisionManager().enabledDebugDraw = true;
+  }
+  get text(): string {
+    return this._text;
+  }
+
+  set text(newVal) {
+    this._text = newVal;
+    this.richTextNode.string = newVal;
+  }
+
+  get soundName(): string {
+    return this._soundName;
+  }
+
+  set soundName(newVal) {
+    this._soundName = newVal;
+    Util.loadGameSound(this._soundName, (clip) => {
+      this._soundClip = clip;
+    });
+  }
+
+  onTouchStart(touch: cc.Touch) {
+    this.isCollisionEnable = true;
+
+    this.originalLocation = touch.currentTarget.position;
+    cc.log("<>" + this.originalLocation);
+    super.onTouchStart(touch);
+    if (this._soundClip != null) {
+      Util.play(this._soundClip, false);
     }
+  }
 
-    set text(newVal) {
-        this._text = newVal;
-        this.richTextNode.string = newVal;
+  onTouchMove(touch: cc.Touch) {
+    super.onTouchMove(touch);
+    this.node.setPosition(this.node.position.x, -cc.winSize.height / 4);
+  }
+
+  onTouchEnd(touch: cc.Touch) {
+    super.onTouchEnd(touch);
+  }
+
+  onCollisionEnter(other: cc.Collider, self: cc.Collider) {
+    cc.log(other.node.x);
+    if (this.isCollisionEnable != false) {
+      other.node.x = this.originalLocation.x;
+      this.isCollisionEnable = false;
     }
+  }
 
-    get soundName(): string {
-        return this._soundName;
-    }
+  onCollisionExit(other: cc.Collider, self: cc.Collider) {}
 
-    set soundName(newVal) {
-        this._soundName = newVal;
-        Util.loadGameSound(this._soundName, (clip) => {
-            this._soundClip = clip;
-        })
-    }
-
-
-    onTouchStart(touch: cc.Touch) {
-        super.onTouchStart(touch);
-        if(this._soundClip != null) {
-            Util.play(this._soundClip, false);
-        }
-    }
-    onTouchMove(touch: cc.Touch) {
-        super.onTouchMove(touch);
-        // console.log(this.node.position.x) //starts at 0,0 at center
-        // console.log(touch.getLocationInView().x-cc.winSize.width/2) //positive value wrt canvas i.e starts at lower left vertex
-         this.node.setPosition(this.node.position.x, touch.getDelta().y)
-          if(this._soundClip != null) {
-            Util.play(this._soundClip, false);
-        }
-    }
-
-    onTouchEnd(touch: cc.Touch) {
-        super.onTouchEnd(touch);
-        if(this.match) {
-            if(this._soundClip != null) {
-                Util.play(this._soundClip, false);
-            }
-        } else {
-            this.node.emit('dragHayWrong')
-        }
-    }
-
-
+ 
 }
