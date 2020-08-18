b98205552fb539e16af68d8f60556d591ac93b76
Shyamal.Upadhyaya <shyamal.upadhyaya@adesa.com>
2020-08-18 11:48:27 +0530

    added school, teacher, child/parent registration options

--- assets/common/scripts/lib/constants.ts
+++ assets/common/scripts/lib/constants.ts
@@ -1,39 +1,48 @@
+export enum DeployMode {
+    Open,
+    Close
+}
+
 export enum Mode {
     Base,
     Home,
-    School
+    School,
+    Teacher,
+    None
 }
-export const MODE: Mode = Mode.Base;
 
-export const LANG = 'en'
-export const ENV = 'stage'
-export const ASSET_LOAD_METHOD = 'file' //'file', 'resources'
+export let D_MODE: DeployMode = DeployMode.Close;
+export let MODE: Mode = Mode.Base;
+
+export const LANG = 'en';
+export const ENV = 'stage';
+export const ASSET_LOAD_METHOD = 'file'; //'file', 'resources'
 // export const ASSET_LOAD_METHOD = 'resources' //'file', 'resources'
 // export const COURSES_URL = 'https://chimple-ee1ed.web.app/courses/'
 // export const COURSES_URL = `https://bahama-${LANG}-${ENV}.web.app/courses/`
 // export const COURSES_URL = 'http://localhost:8901/courses/'
-export const ANDROID_ROOT_DIR = '/sdcard/bahama/'
-export const SIMULATOR_ROOT_DIR = '/Users/shyamalupadhyaya/Dev/chimple-git/sdcard/bahama/'
+export const ANDROID_ROOT_DIR = '/sdcard/bahama/';
+export const SIMULATOR_ROOT_DIR = '/Users/shyamalupadhyaya/Dev/chimple-git/sdcard/bahama/';
 
 export const COURSE_SERVER = {
-    'dev': {
-        'hi' : 'http://localhost:8901/courses/',
-        'en' : 'http://localhost:8901/courses/',
+    'dev'  : {
+        'hi'      : 'http://localhost:8901/courses/',
+        'en'      : 'http://localhost:8901/courses/',
         'en-maths': 'http://localhost:8901/courses/',
-        'hi-maths' : 'http://localhost:8901/courses/',
+        'hi-maths': 'http://localhost:8901/courses/'
     },
     'stage': {
-        'hi' : 'https://bahama-hi-stage.web.app/new/courses/',
-        'en' : 'https://bahama-hi-stage.web.app/new/courses/',
+        'hi'      : 'https://bahama-hi-stage.web.app/new/courses/',
+        'en'      : 'https://bahama-hi-stage.web.app/new/courses/',
         'en-maths': 'https://bahama-hi-stage.web.app/new/courses/',
-        'hi-maths' : 'https://bahama-hi-stage.web.app/new/courses/',
+        'hi-maths': 'https://bahama-hi-stage.web.app/new/courses/'
     },
-    'prod': {
-        'hi' : 'https://bahama-hi-prod.web.app/courses/',
-        'en' : 'https://bahama-hi-prod.web.app/courses/',
+    'prod' : {
+        'hi'      : 'https://bahama-hi-prod.web.app/courses/',
+        'en'      : 'https://bahama-hi-prod.web.app/courses/',
         'en-maths': 'https://bahama-hi-prod.web.app/courses/',
-        'hi-maths' : 'https://bahama-hi-prod.web.app/courses/',
+        'hi-maths': 'https://bahama-hi-prod.web.app/courses/'
     }
-}
+};
 
-export const COURSES_URL = COURSE_SERVER[ENV][LANG]
+export const COURSES_URL = COURSE_SERVER[ENV][LANG];
