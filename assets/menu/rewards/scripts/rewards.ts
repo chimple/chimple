d0f5600f8fd05cb0927f4352d1c0b28caf1dc31a
Rajesh <kumar@sutara.org>
2020-10-14 18:03:28 +0530

    Revert "Removed elastic property ."
    This reverts commit 9d17a02f5445cc4907c703395a2ac4e552a5c3bc.

--- assets/menu/rewards/scripts/rewards.ts
+++ assets/menu/rewards/scripts/rewards.ts
@@ -53,16 +53,16 @@ export default class Rewards extends cc.Component {
                     charPrefab.getChildByName("character_icon_color").color = color.fromHEX(this.characterColors[index]);
                     this.registerButton(charPrefab, "onCharacterClick", character);
                     this.registerButton(charPrefab.getChildByName("edit"), "onEditButtonClicked", character);
-                    // if (User.getCurrentUser().unlockedRewards[`${REWARD_TYPES[0]}-${character}`] === 0 || User.getCurrentUser().unlockedRewards[`${REWARD_TYPES[0]}-${character}`] === undefined) {
-                    //     // make lock texture active
-                    //     charPrefab.getChildByName("lock").active = true
-                    //     charPrefab.getComponent(cc.Button).interactable = false
-                    // }
-                    // if (character === User.getCurrentUser().currentCharacter) {
-                    //     // make edit button and selected show
-                    //     charPrefab.getChildByName("tick").active = true
-                    //     charPrefab.getChildByName("edit").active = true
-                    // }
+                    if (User.getCurrentUser().unlockedRewards[`${REWARD_TYPES[0]}-${character}`] === 0 || User.getCurrentUser().unlockedRewards[`${REWARD_TYPES[0]}-${character}`] === undefined) {
+                        // make lock texture active
+                        charPrefab.getChildByName("lock").active = true
+                        charPrefab.getComponent(cc.Button).interactable = false
+                    }
+                    if (character === User.getCurrentUser().currentCharacter) {
+                        // make edit button and selected show
+                        charPrefab.getChildByName("tick").active = true
+                        charPrefab.getChildByName("edit").active = true
+                    }
                     const characterNode = this.layoutHolder.children[0].children[0].children[0].children[0]
                     characterNode.addChild(charPrefab);
                 }
