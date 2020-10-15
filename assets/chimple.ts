6228b591d415fc9b005791b98282f63018cb5a41
Rajesh <kumar@sutara.org>
2020-10-15 21:36:50 +0530

    Update chimple.ts

--- assets/chimple.ts
+++ assets/chimple.ts
@@ -1,10 +1,10 @@
-import Config, {LANG_CONFIGS, Lang} from "./common/scripts/lib/config";
-import Profile, {Gender, User, LANGUAGE} from "./common/scripts/lib/profile";
-import {D_MODE, DeployMode, Mode, MODE} from "./common/scripts/lib/constants";
+import Config, { LANG_CONFIGS, Lang } from "./common/scripts/lib/config";
+import Profile, { Gender, User, LANGUAGE } from "./common/scripts/lib/profile";
+import { D_MODE, DeployMode, Mode, MODE } from "./common/scripts/lib/constants";
 import UtilLogger from "./common/scripts/util-logger";
-import {Util} from "./common/scripts/util";
+import { Util } from "./common/scripts/util";
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 export const CHIMPLE_MODE = 'CHIMPLE_MODE';
 export const DEPLOY_MODE = 'DEPLOY_MODE';
@@ -37,41 +37,41 @@ export const START_SCENE = 'menu/start/scenes/start';
 cc.deep_link = function (url) {
     cc.log("deep link called with url:" + url);
     http://chimple.github.io
-        if (url !== null && url.includes("http://chimple.github.io/")) {
-            let messageType: string = null;
-            let splits = url.split("://chimple.github.io/");
-            if (splits !== null && splits.length === 2) {
-                let elements = splits[1].split('/');
-                messageType = elements.splice(0, 1)[0];
-                if (messageType === RECEIVED_TEACHER_REQUEST) {
-                    let data = Object.assign({});
-                    if (elements !== null && (elements.length % 2 === 0)) {
-                        let all_keys = elements;
-                        let all_values = [];
-                        for (let i = 0; i < elements.length; i++) {
-                            all_values.push(all_keys.splice(i + 1, 1)[0]);
+    if (url !== null && url.includes("http://chimple.github.io/")) {
+        let messageType: string = null;
+        let splits = url.split("://chimple.github.io/");
+        if (splits !== null && splits.length === 2) {
+            let elements = splits[1].split('/');
+            messageType = elements.splice(0, 1)[0];
+            if (messageType === RECEIVED_TEACHER_REQUEST) {
+                let data = Object.assign({});
+                if (elements !== null && (elements.length % 2 === 0)) {
+                    let all_keys = elements;
+                    let all_values = [];
+                    for (let i = 0; i < elements.length; i++) {
+                        all_values.push(all_keys.splice(i + 1, 1)[0]);
+                    }
+                    let mappings = all_keys.map(function (e, i) {
+                        return [e, all_values[i]];
+                    });
+
+                    mappings.forEach(arr => {
+                        if (arr && arr.length === 2) {
+                            data[arr[0].toLowerCase()] = arr[1]
                         }
-                        let mappings = all_keys.map(function (e, i) {
-                            return [e, all_values[i]];
-                        });
+                    })
+                }
+                try {
+                    const jsonMessages: any[] = Util.removeDuplicateMessages(data, messageType);
+                    UtilLogger.logChimpleEvent(RECEIVED_TEACHER_REQUEST, data);
+                    cc.sys.localStorage.setItem(messageType, JSON.stringify(jsonMessages));
+                } catch (e) {
 
-                        mappings.forEach(arr => {
-                            if (arr && arr.length === 2) {
-                                data[arr[0].toLowerCase()] = arr[1]
-                            }
-                        })
-                    }
-                    try {
-                        const jsonMessages: any[] = Util.removeDuplicateMessages(data, messageType);
-                        UtilLogger.logChimpleEvent(RECEIVED_TEACHER_REQUEST, data);
-                        cc.sys.localStorage.setItem(messageType, JSON.stringify(jsonMessages));
-                    } catch (e) {
-
-                    }
                 }
             }
-            cc.log('saved into local storage:' + cc.sys.localStorage.getItem(messageType));
         }
+        cc.log('saved into local storage:' + cc.sys.localStorage.getItem(messageType));
+    }
 };
 
 @ccclass
@@ -98,6 +98,20 @@ export default class Chimple extends cc.Component {
         }
         // this.navigateToBase();
 
+    }
+
+    private selectModes() {
+        const modes: number = MODE;
+        switch (modes) {
+            case Mode.Home:
+                // send to welcomePage.ts
+                Config.i.pushScene('private/home/loginnew/scenes/welcomePage', 'private', null, true);
+                break;
+            case Mode.School:
+                // send to selectSections.ts
+                Config.i.pushScene('private/schools/scenes/selectSections', 'private', null, true);
+                break;
+        }
     }
 
     public static navigateToHome() {
