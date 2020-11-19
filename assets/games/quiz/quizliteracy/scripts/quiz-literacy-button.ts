2afef6df4ce1360254430d8d8a9d42204be9e453
shyamalu <shyamal@sutara.org>
2020-11-19 13:25:01 +0530

    Fixes #739

--- assets/games/quiz/quizliteracy/scripts/quiz-literacy-button.ts
+++ assets/games/quiz/quizliteracy/scripts/quiz-literacy-button.ts
@@ -1,6 +1,7 @@
 import { Util } from "../../../../common/scripts/util";
 import catchError from "../../../../common/scripts/lib/error-handler";
 import { QuizBtnData, QuizBtnType, QUIZ_CORRECT, QUIZ_WRONG } from "./quiz-literacy";
+import isAbsolutePath = jsb.fileUtils.isAbsolutePath;
 
 const {ccclass, property} = cc._decorator;
 
@@ -27,17 +28,33 @@ export default class QuizLiteracyButton extends cc.Component {
             const picWidth = this.node.width;
             const picHeight = this.node.height;
 
-            Util.loadTexture(this.quizDir + this.data.pic, (texture) => {
-                if (texture != null) {
-                    this.image.spriteFrame = new cc.SpriteFrame(texture);
-                    const size = this.image.spriteFrame.getOriginalSize();
-                    const xScale = picWidth / size.width;
-                    const yScale = picHeight / size.height;
-                    const scale = Math.min(xScale, yScale);
-                    this.node.width = scale * size.width;
-                    this.node.height = scale * size.height;
-                }
-            });
+            if(this.data.absolutePath) {
+                cc.resources.load(this.quizDir + this.data.pic, cc.SpriteFrame, (err, spriteFrame) => {
+                    if (!err) {
+                        // @ts-ignore
+                        this.image.spriteFrame = spriteFrame;
+                        const size = this.image.spriteFrame.getOriginalSize();
+                        const xScale = picWidth / size.width;
+                        const yScale = picHeight / size.height;
+                        const scale = Math.min(xScale, yScale);
+                        this.node.width = scale * size.width;
+                        this.node.height = scale * size.height;
+                    }
+                });
+
+            } else {
+                Util.loadTexture(this.quizDir + this.data.pic, (texture) => {
+                    if (texture != null) {
+                        this.image.spriteFrame = new cc.SpriteFrame(texture);
+                        const size = this.image.spriteFrame.getOriginalSize();
+                        const xScale = picWidth / size.width;
+                        const yScale = picHeight / size.height;
+                        const scale = Math.min(xScale, yScale);
+                        this.node.width = scale * size.width;
+                        this.node.height = scale * size.height;
+                    }
+                });
+            }
         }
     }
 
