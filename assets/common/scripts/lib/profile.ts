36c2e2b7aed3c2f462ad1c279e90303de96f113e
Srikanth Talapadi <sri@sutara.org>
2020-12-30 11:50:38 +0530

    Added some translations and removed hot update

--- assets/common/scripts/lib/profile.ts
+++ assets/common/scripts/lib/profile.ts
@@ -14,7 +14,7 @@ export const GENDER = "gender";
 export const MUSIC_OFF = "musicOff";
 export const AGE = "age";
 export const USER_ID = "UserId";
-export const MAX_USERS = 3;
+export const MAX_USERS = 4;
 export const MAX_AGE = 12;
 export const LANGUAGE = "language";
 export const CURRENTMODE = 'currentMode';
@@ -589,17 +589,11 @@ export class User {
             "chimp",
             debug
                 ? new Map([
-                    ['en', new CourseProgressClass('en00')],
                     ['maths', new CourseProgressClass('maths00')],
-                    ['hi', new CourseProgressClass('hi00')],
-                    ['puzzle', new CourseProgressClass('puzzle00')],
-                    ['test-lit', new CourseProgressClass('chapter_0')],
-                    ['test-maths', new CourseProgressClass('chapter_0')]
+                    ['puzzle', new CourseProgressClass('puzzle00')]
                 ])
                 : new Map([
-                    ['en', new CourseProgressClass()],
                     ['maths', new CourseProgressClass()],
-                    ['hi', new CourseProgressClass('hi00')],
                     ['puzzle', new CourseProgressClass('puzzle00')]
                 ]),
             new Map(),
