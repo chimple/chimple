46d831a8784ec706850fb536656cc4c0276a3576
vinay961861 <vinay@sutara.org>
2021-02-04 21:06:23 +0530

    changes

--- assets/common/scripts/webLesson.ts
+++ assets/common/scripts/webLesson.ts
@@ -11,22 +11,7 @@ const {ccclass, property} = cc._decorator;
 export default class WebLesson extends cc.Component {
     onLoad () {
         const params = new URLSearchParams(window.location.search)
-        const courseId = params.get('courseId')
-        const chapterId = params.get('chapterId')
-        const lessonId = params.get('lessonId')
-        const config = Config.i
-        config.isMicroLink = true
-        config.loadSingleCourseJson(courseId, () => {
-            config.course = config.curriculum.get(courseId)
-            config.chapter = config.course.chapters.find((c) => c.id == chapterId)
-            config.lesson = config.chapter.lessons.find((l) => l.id == lessonId)
-            LessonController.preloadLesson(this.node, (err: Error) => {
-                if(err) {
-                    console.log(err)
-                } else {
-                    Config.loadScene('common/scenes/lessonController')
-                }
-            })    
-        })
+        Config.i.isMicroLink = true
+        Util.loadDirectLessonWithLink(params.get('courseId'),params.get('chapterId'),params.get('lessonId'),this.node);
     }
 }
