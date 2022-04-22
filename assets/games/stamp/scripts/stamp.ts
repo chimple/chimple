2e1c3f83e115f9802a51457959b8f4ac4b622652
sutara-skanda <62737989+Sutara-Skanda@users.noreply.github.com>
2022-04-22 09:00:55 +0530

    Added Screenshot Code

--- assets/games/stamp/scripts/stamp.ts
+++ assets/games/stamp/scripts/stamp.ts
@@ -41,6 +41,9 @@ export default class Stamp extends Game {
     @property(cc.Node)
     mask: cc.Node = null
 
+    @property(cc.Node)
+    camera: cc.Node = null
+
     numPieces: number = 0;
     text: string = null
     audio: cc.AudioClip = null
@@ -268,4 +271,113 @@ export default class Stamp extends Game {
             this.graphics.stroke()
         }
     }
+
+
+    _width: 0
+    _height: 0
+    texture
+
+    capturingScreenshot() {
+        // this.mask.enabled = false//.node.active = false
+        // this.mask.node.setContentSize(0, 0)
+
+        console.log('screenshot method called')
+        let texture = new cc.RenderTexture();
+        texture.initWithSize(cc.visibleRect.width, cc.visibleRect.height, cc.Texture2D.PixelFormat.RGBA8888);
+        this.camera.getComponent(cc.Camera).targetTexture = texture;
+        this.texture = texture;
+        // create the capture
+        this.schedule(() => {
+            let picData = this.initImage();
+            this.createCanvas(picData);
+            // this.label.string = 'Showing the capture'
+            this.saveFile(picData);
+            // this.camera.enabled = false;
+        }, 1, 0);
+    };
+
+    // override
+    initImage() {
+        let data = this.texture.readPixels();
+        this._width = this.texture.width;
+        this._height = this.texture.height;
+        let picData = this.filpYImage(data, this._width, this._height);
+        return picData;
+    };
+
+    // This is a temporary solution
+    filpYImage(data, width, height) {
+        // create the data array
+        let picData = new Uint8Array(width * height * 4);
+        let rowBytes = width * 4;
+        for (let row = 0; row < height; row++) {
+            let srow = height - 1 - row;
+            let start = srow * width * 4;
+            let reStart = row * width * 4;
+            // save the piexls data
+            for (let i = 0; i < rowBytes; i++) {
+                picData[reStart + i] = data[start + i];
+            }
+        }
+        return picData;
+    }
+
+    // override init with Data
+    createCanvas(picData) {
+        let texture = new cc.Texture2D();
+        texture.initWithData(picData, 32, this._width, this._height);
+
+        let spriteFrame = new cc.SpriteFrame();
+        spriteFrame.setTexture(texture);
+
+        let node = new cc.Node();
+        let sprite = node.addComponent(cc.Sprite);
+        sprite.spriteFrame = spriteFrame;
+
+        node.zIndex = cc.macro.MAX_ZINDEX;
+        node.parent = cc.director.getScene();
+        // set position
+        let width = cc.winSize.width;
+        let height = cc.winSize.height;
+        node.x = width / 2;
+        node.y = height / 2;
+        node.on(cc.Node.EventType.TOUCH_START, () => {
+            node.parent = null;
+            // this.label.string = '';
+            node.destroy();
+        });
+
+        this.captureAction(node, width, height);
+    }
+
+    // sprite action
+    captureAction(capture, width, height) {
+        let scaleAction = cc.scaleTo(1, 0.3);
+        let targetPos = cc.v2(width - width / 6, height / 4);
+        let moveAction = cc.moveTo(1, targetPos);
+        let spawn = cc.spawn(scaleAction, moveAction);
+        capture.runAction(spawn);
+        let blinkAction = cc.blink(0.1, 1);
+        // scene action
+        this.node.runAction(blinkAction);
+    }
+
+    saveFile(picData) {
+        cc.log("saveFile() called");
+        if (CC_JSB) {
+            cc.log("saveFile() Entered if(CC_JSB)");
+            let filePath = jsb.fileUtils.getWritablePath() + 'render_to_sprite_image.png';
+
+            // @ts-ignore
+            let success = jsb.saveImageData(picData, this._width, this._height, filePath)
+            cc.log("saveFile() success", success);
+            if (success) {
+                cc.log("save image data success, file: " + filePath);
+            }
+            else {
+                cc.error("save image data failed!");
+            }
+        }
+    };
+
 }
