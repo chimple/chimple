72a49d397cf6dd1da046d04b61638a07efbfab33
sutara-skanda <62737989+Sutara-Skanda@users.noreply.github.com>
2023-03-07 08:19:20 +0530

    Removed Unnecessary code in OpenWindow1

--- assets/games/openwindow1/scripts/openwindow1-slot.ts
+++
@@ -1,6 +0,0 @@
-import ccclass = cc._decorator.ccclass;
-
-@ccclass
-export default class OpenWindow1Slot extends cc.Component {
-
-}
--- assets/games/openwindow1/scripts/openwindow1.ts
+++ assets/games/openwindow1/scripts/openwindow1.ts
@@ -31,12 +31,6 @@ export default class OpenWindow1 extends Game {
     @property(cc.Prefab)
     slotWindowPrefab: cc.Prefab = null;
 
-    @property(cc.Prefab)
-    scrollWindow: cc.Prefab = null;
-
-    @property(cc.Prefab)
-    slotFramePrefab: cc.Prefab = null;
-
     @property(cc.AudioClip)
     scrollClip: cc.AudioClip = null;
 
@@ -47,11 +41,8 @@ export default class OpenWindow1 extends Game {
     wrongClip: cc.AudioClip = null;
 
     private _frameHeight: number = 0;
-    private _slots: cc.Node[] = [];
-    private _words: string[] = [];
     private _choiceCard1: cc.Node = null;
     private _choiceCard2: cc.Node = null;
-    private _curIndex: number = 0;
     private _helpDragNode: cc.Node = null;
 
     _isRTL: boolean = false;
@@ -64,7 +55,6 @@ export default class OpenWindow1 extends Game {
         this.buildUI();
         this.node.on(START_SCROLL_CLICK, (event) => {
             event.stopPropagation();
-            this._curIndex = 0;
             this.renderUI();
         });
     }
@@ -77,7 +67,7 @@ export default class OpenWindow1 extends Game {
 
     @catchError()
     buildUI() {
-        this.buildSlots();
+        this.buildWord();
         this.buildChoices();
         this.renderUI();
 
@@ -107,49 +97,8 @@ export default class OpenWindow1 extends Game {
         });
     }
 
-    // playCloseAnim() {
-    //     const curtain = this._randomWindow.getChildByName('curtain_node');
-    //     if (!!curtain) {
-    //         const anim: cc.Animation = curtain.getComponent(cc.Animation);
-    //         anim.play('close');
-    //         this.scheduleOnce(() => {
-    //             this.playOpenAnim();
-    //             try {
-    //                 // @ts-ignore
-    //                 Util.loadGameSound(this._currentConfig.sound, (clip) => {
-    //                     if(clip) {
-    //                         Util.play(clip, false);
-    //                     }
-    //                     this.scheduleOnce(
-    //                         () => {
-    //                             this.node.emit('nextProblem');
-    //                         }, 1
-    //                     );
-    //                 });
-    //             } catch (e) {
-    //
-    //             }
-    //         }, 1);
-    //     }
-    // }
-
     @catchError()
-    playOpenAnim(scrollableSlot: cc.Node, word: string) {
-        const curtain = scrollableSlot.getChildByName('curtain_node');
-        if (!!curtain) {
-            const anim: cc.Animation = curtain.getComponent(cc.Animation);
-            anim.play('open');
-            anim.on('finished', () => {
-                this.speak(word, () => {
-                    this._curIndex++;
-                    this.renderUI();
-                });
-            });
-        }
-    }
-
-    @catchError()
-    buildSlots() {
+    buildWord() {
         const words = this._currentConfig.word.indexOf(',') !== -1 ?
             this._currentConfig.word.split(',') :
             this._currentConfig.word.split('');
@@ -165,70 +114,38 @@ export default class OpenWindow1 extends Game {
             layoutComponent.resizeMode = cc.Layout.ResizeMode.CONTAINER;
         }
         if (Config.wide) layoutComponent.spacingX = 45
-        this.addFrames(slotLayout, words);
+
+        // const color = new cc.Color().fromHEX('#' + Math.floor(Math.random() * 16777215).toString(16));
+        const labelNode = slotLayout.getChildByName('label');
+        // labelNode.color = color;
+        const label = labelNode.getComponent(cc.Label);
+        label.string = this._currentConfig.word;
         this.node.addChild(slotWindow);
-        this._slots = this._slots.reverse();
-        this._words = this._words.reverse();
     }
 
-    @catchError()
-    addSlots(parent: cc.Node, word: string) {
-        const scrollableWindow = cc.instantiate(this.scrollWindow);
-        const contentSlot = scrollableWindow.getChildByName('contentNode');
-        this.addContent(contentSlot, word);
-        const curtainNode = scrollableWindow.getChildByName('curtain_node');
-        if (Config.wide) curtainNode.scale = 1.4
-        parent.addChild(scrollableWindow);
-        this._slots.push(scrollableWindow);
-        this._words.push(word);
-    }
 
     @catchError()
     renderUI() {
-        const popIndex = this._curIndex <= this._slots.length - 1 ?
-            this._isRTL ? this._curIndex : this._slots.length - 1 - this._curIndex : -1;
-        if (popIndex !== -1) {
-            const slot = this._slots[popIndex];
-            const word = this._words[popIndex];
-            if (!!slot && !!word) {
-                this.animationOpenWindow(slot, word);
+        Util.loadGameSound(this._currentConfig.sound, (clip) => {
+            if (clip) {
+                // Util.play(clip, false);
+                this.friend.extraClip = clip
             }
-        } else {
-            Util.loadGameSound(this._currentConfig.sound, (clip) => {
-                if (clip) {
-                    // Util.play(clip, false);
-                    this.friend.extraClip = clip
-                }
-                this.scheduleOnce(
-                    () => {
-                        this._choiceCard1.parent.getComponent(OpenWindow1ChoiceCard).touchEnabled = true;
-                        this._choiceCard2.parent.getComponent(OpenWindow1ChoiceCard).touchEnabled = true;
-                        this._choiceCard1.opacity = 255;
-                        this._choiceCard2.opacity = 255;
-                        this.scheduleOnce(
-                            () => {
-                                Util.showHelp(this._helpDragNode, this._helpDragNode);
-                            }, 0.5
-                        );
+            this.scheduleOnce(
+                () => {
+                    this._choiceCard1.parent.getComponent(OpenWindow1ChoiceCard).touchEnabled = true;
+                    this._choiceCard2.parent.getComponent(OpenWindow1ChoiceCard).touchEnabled = true;
+                    this._choiceCard1.opacity = 255;
+                    this._choiceCard2.opacity = 255;
+                    this.scheduleOnce(
+                        () => {
+                            Util.showHelp(this._helpDragNode, this._helpDragNode);
+                        }, 0.5
+                    );
 
-                    }, 1
-                );
-            });
-        }
-    }
-
-    @catchError()
-    animationOpenWindow(scrollableSlot: cc.Node, word: string) {
-        this.scheduleOnce(
-            () => {
-                this.playOpenAnim(scrollableSlot, word);
-            }, 0.5
-        );
-    }
-
-    @catchError()
-    speak(w: string, callBack: Function) {
-        this.friend.speakPhonicsOrLetter(w, callBack);
+                }, 1
+            );
+        });
     }
 
     @catchError()
@@ -243,15 +160,6 @@ export default class OpenWindow1 extends Game {
                 scrollView.scrollToOffset(new cc.Vec2(0, index * this._frameHeight), 0.5);
             }, 0.5);
         }
-
-        this.scheduleOnce(
-            () => {
-                this.speak(word, () => {
-                    this._curIndex++;
-                    this.renderUI();
-                });
-            }, 1
-        );
     }
 
     @catchError()
@@ -265,52 +173,6 @@ export default class OpenWindow1 extends Game {
             this._currentConfig.word.split('');
         slotItems = slotItems.concat(answers);
         return Array.from(new Set(slotItems));
-    }
-
-    @catchError()
-    addScrollContents(parent: cc.Node) {
-        const color = new cc.Color().fromHEX('#' + Math.floor(Math.random() * 16777215).toString(16));
-        let slotItems = this.getSlotItems();
-
-        slotItems.forEach(
-            s => {
-                const frame = this.createSlotFrame(s, color);
-                parent.addChild(frame);
-                this._frameHeight = frame.height;
-                parent.height += frame.height;
-            }
-        );
-    }
-
-    @catchError()
-    addContent(parent: cc.Node, s: string) {
-        const color = new cc.Color().fromHEX('#' + Math.floor(Math.random() * 16777215).toString(16));
-        const frame = this.createSlotFrame(s, color);
-        parent.addChild(frame);
-        s;
-    }
-
-    @catchError()
-    createSlotFrame(w: string, color: cc.Color): cc.Node {
-        const frame = cc.instantiate(this.slotFramePrefab);
-        const slotItem = frame.getChildByName('slotitem');
-        const labelNode = slotItem.getChildByName('label');
-        labelNode.color = color;
-        const label = labelNode.getComponent(cc.Label);
-        label.string = Config.wide ? ' ' + w + ' ' : w;
-        const outLine = labelNode.addComponent(cc.LabelOutline);
-        outLine.width = 2;
-
-        return frame;
-    }
-
-    @catchError()
-    addFrames(parent: cc.Node, words: string[]) {
-        words.forEach(
-            w => {
-                this.addSlots(parent, w);
-            }
-        );
     }
 
     @catchError()
