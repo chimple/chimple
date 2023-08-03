cc988e58f8b907a20c65fac6d9711a362d18a12b
ALOK-TV <alok@sutara.org>
2023-08-03 12:25:16 +0530

    Adding SL curriculum to Bahama

--- build/jsb-default/frameworks/runtime-src/proj.android-studio/firebaseSync/src/main/java/org/chimple/firebasesync/auth/AuthCallBack.java
+++
@@ -1,6 +0,0 @@
-package org.chimple.firebasesync.auth;
-
-public interface AuthCallBack {
-    public void loginSucceed(String schoolInfo, boolean shouldCallBack);
-    public void loginFailed(String reason);
-}
